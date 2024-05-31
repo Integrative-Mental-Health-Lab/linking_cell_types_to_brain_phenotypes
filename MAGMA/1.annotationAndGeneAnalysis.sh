245ce9671f414270896c2c24ab7d5a31e0b25668
taydenli <taydenli09876@gmail.com>
2024-05-31 16:20:10 -0700

    minor code edits to improve clarity

--- MAGMA/1.annotationAndGeneAnalysis.sh
+++ MAGMA/1.annotationAndGeneAnalysis.sh
@@ -3,15 +3,15 @@
 #SBATCH --mem=16G
 
 ml gcc
-folder="MultipleSclerosis"
-file="discovery_metav3.0.meta"
-snp_col="SNP"
-p_col="P"
-N=41505
+folder="SCZ"
+file="PGC3_SCZ_wave3.european.autosome.public.v3.vcf.tsv.no_heading"
+snp_col="ID"
+p_col="PVAL"
+ncol="NEFF"
 
 MAGMA_v1.10/magma --annotate window=35,10 \
 --snp-loc ${folder}/snploc_${file} \
 --gene-loc auxfiles/NCBI37.3.gene.loc \
 --out ${folder}/${file}.step1
 
-MAGMA_v1.10/magma --bfile auxfiles/g1000_eur --pval ${folder}/${file} use=${snp_col},${p_col} N=${N} --gene-annot ${folder}/${file}.step1.genes.annot --out ${folder}/${file}.step2
+MAGMA_v1.10/magma --bfile auxfiles/g1000_eur --pval ${folder}/${file} use=${snp_col},${p_col} ncol=${ncol} --gene-annot ${folder}/${file}.step1.genes.annot --out ${folder}/${file}.step2
--- MAGMA/4.conditionalAnalysis.sh
+++ MAGMA/4.conditionalAnalysis.sh
@@ -3,11 +3,11 @@
 #SBATCH --mem=16G
 
 ml gcc
-folder="MultipleSclerosis"
-file="discovery_metav3.0.meta"
-gwas_name="MultipleSclerosis_2018"
-covar_file_spe="Siletti_l2_specificity_matrix_50-only"
-out_file_spe="Siletti_l2_joint_spe_50-only"
+folder="SCZ"
+file="PGC3_SCZ_wave3.european.autosome.public.v3.vcf.tsv.no_heading"
+gwas_name="SCZ_2022"
+covar_file_spe="Siletti_l2_specificity_matrix_sig-only"
+out_file_spe="Siletti_l2_joint_spe_sig-only"
 
 MAGMA_v1.10/magma --gene-results ${folder}/${file}.step2.genes.raw --gene-covar ${folder}/${covar_file_spe}_${gwas_name}.txt --model joint-pairs direction=greater --out ${folder}/${out_file_spe}_${gwas_name}
 
